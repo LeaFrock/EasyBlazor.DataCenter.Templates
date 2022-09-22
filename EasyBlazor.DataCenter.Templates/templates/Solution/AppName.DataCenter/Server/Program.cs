58e3e3dcc55360d501dbdd1a2b0e6edb3aa79319
LeaFrock <petergeorge@126.com>
2022-09-22 11:12:42 +0800

    Remove Startup.cs

--- EasyBlazor.DataCenter.Templates/templates/Solution/AppName.DataCenter/Server/Program.cs
+++ EasyBlazor.DataCenter.Templates/templates/Solution/AppName.DataCenter/Server/Program.cs
@@ -1,4 +1,6 @@
-﻿using NLog;
+﻿using Microsoft.AspNetCore.DataProtection;
+using Microsoft.AspNetCore.HttpOverrides;
+using NLog;
 using NLog.Web;
 
 namespace AppName.DataCenter.Server
@@ -10,8 +12,64 @@ namespace AppName.DataCenter.Server
             var logger = LogManager.Setup().LoadConfigurationFromAppSettings().GetCurrentClassLogger();
             try
             {
-                logger.Debug("init main");
-                CreateHostBuilder(args).Build().Run();
+                var builder = WebApplication.CreateBuilder(args);
+
+                builder.Services.AddControllersWithViews();
+                builder.Services.AddRazorPages();
+                builder.Services.AddDataProtection()
+                    .PersistKeysToFileSystem(new(Path.Combine(builder.Environment.ContentRootPath, "DataProtectionKeys")));
+
+                string connStr = builder.Configuration.GetConnectionString("SqlServer");
+                builder.Services.AddAuthModule(builder.Configuration);
+                builder.Services.AddEntityFrameworkMySqlModule(connStr);
+                builder.Services.AddAutoMapper();
+                builder.Services.AddInternalModule();
+
+#if RELEASE
+                builder.Logging.ClearProviders();
+#endif
+                builder.Host.UseNLog();
+
+                var app = builder.Build();
+                if (app.Environment.IsDevelopment())
+                {
+                    app.UseDeveloperExceptionPage();
+                    app.UseWebAssemblyDebugging();
+                }
+                else
+                {
+                    app.UseExceptionHandler("/Error");
+                    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                    app.UseHsts();
+                }
+
+                app.UseForwardedHeaders(new()
+                {
+                    ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
+                });
+                string pathBase = app.Configuration.GetSection("Urls")?.GetValue<string>("PathBase");
+                if (!string.IsNullOrEmpty(pathBase))
+                {
+                    app.UsePathBase(pathBase);
+                }
+
+                //app.UseHttpsRedirection();
+                app.UseBlazorFrameworkFiles();
+                app.UseStaticFiles();
+
+                app.UseRouting();
+
+                app.UseAuthentication()
+                    .UseAuthorization();
+
+                app.UseEndpoints(endpoints =>
+                {
+                    endpoints.MapRazorPages();
+                    endpoints.MapControllers();
+                    endpoints.MapFallbackToFile("index.html");
+                });
+
+                app.Run();
             }
             catch (Exception exception)
             {
@@ -22,23 +80,8 @@ namespace AppName.DataCenter.Server
             finally
             {
                 // Ensure to flush and stop internal timers/threads before application-exit (Avoid segmentation fault on Linux)
-                NLog.LogManager.Shutdown();
+                LogManager.Shutdown();
             }
         }
-
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                })
-                .ConfigureLogging(logging =>
-                {
-#if RELEASE
-                    logging.ClearProviders();
-#endif
-                    logging.SetMinimumLevel(Microsoft.Extensions.Logging.LogLevel.Trace);
-                })
-                .UseNLog();  // NLog: Setup NLog for Dependency injection;
     }
 }
